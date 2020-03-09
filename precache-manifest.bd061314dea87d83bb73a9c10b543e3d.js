301dddff42cf822f1b0e00673c9e23a259994417
Alexander Shepelin <brightcor@gmail.com>
2020-03-09 23:43:03 +0300

    Updates

---
+++ precache-manifest.bd061314dea87d83bb73a9c10b543e3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b63ac8406ed2fe4d92924751bf8a12",
+    "url": "/metric-ai-test/index.html"
+  },
+  {
+    "revision": "3494e3ea17e280d3d8b7",
+    "url": "/metric-ai-test/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "d982bf581f8e5a323c0a",
+    "url": "/metric-ai-test/static/css/main.56fc3dbe.chunk.css"
+  },
+  {
+    "revision": "3494e3ea17e280d3d8b7",
+    "url": "/metric-ai-test/static/js/2.12557226.chunk.js"
+  },
+  {
+    "revision": "f286d1a9ce6e5be678869cc892d2a512",
+    "url": "/metric-ai-test/static/js/2.12557226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d982bf581f8e5a323c0a",
+    "url": "/metric-ai-test/static/js/main.af395cbd.chunk.js"
+  },
+  {
+    "revision": "47c0d78e328c0ca94234",
+    "url": "/metric-ai-test/static/js/runtime-main.7b9365a1.js"
+  }
+]);
